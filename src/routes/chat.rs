b11fd2b0c2c0c470c02bf236cb093a21d9f56a10
neil-ca <ulisescamps9@gmail.com>
2023-10-21 16:40:48 -0600

    Building the server with actors for the chat

--- src/routes/chat.rs
+++ src/routes/chat.rs
@@ -1,25 +1,190 @@
-use actix::{Actor, StreamHandler};
+use actix::prelude::*;
 use actix_web::{web, Error, HttpRequest, HttpResponse};
 use actix_web_actors::ws;
+use rand::{self, rngs::ThreadRng, Rng};
+use std::{
+    collections::{HashMap, HashSet},
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    time::{Duration, Instant},
+};
 
-struct MyWs;
-impl Actor for MyWs {
-    type Context = ws::WebsocketContext<Self>;
+// Server
+// Chat server sends this message to session
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Message(pub String);
+
+// New chat session is created
+#[derive(Message)]
+#[rtype(usize)]
+pub struct Connect {
+    pub addr: Recipient<Message>,
 }
 
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
-    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
+// Session is disconected
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Disconnect {
+    pub id: usize,
+}
+
+// Send message to specific room
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct ClientMessage {
+    pub id: usize,
+    pub msg: String,
+    pub room: String,
+}
+
+pub struct ListRooms;
+
+impl actix::Message for ListRooms {
+    type Result = Vec<String>;
+}
+
+// Join room, if room does not exists create new one.
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct Join {
+    pub id: usize,
+    pub name: String,
+}
+
+// `ChatServer` manages chat rooms and responsible for coordinating chat session
+pub struct ChatServer {
+    sessions: HashMap<usize, Recipient<Message>>,
+    rooms: HashMap<String, HashSet<usize>>,
+    rng: ThreadRng,
+    visitor_count: Arc<AtomicUsize>,
+}
+
+impl ChatServer {
+    pub fn new(visitor_count: Arc<AtomicUsize>) -> ChatServer {
+        let mut rooms = HashMap::new();
+        rooms.insert("main".to_owned(), HashSet::new());
+        ChatServer {
+            sessions: HashMap::new(),
+            rooms,
+            rng: rand::thread_rng(),
+            visitor_count,
         }
     }
 }
 
-pub async fn chat(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    ws::start(MyWs {}, &req, stream)
-    // println!("{:?}", resp);
-    // resp
+impl ChatServer {
+    fn send_message(&self, room: &str, message: &str, skip_id: usize) {
+        if let Some(sessions) = self.rooms.get(room) {
+            for id in sessions {
+                if *id != skip_id {
+                    if let Some(addr) = self.sessions.get(id) {
+                        addr.do_send(Message(message.to_owned()));
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl Actor for ChatServer {
+    // Simple Context to communicate with other actors.
+    type Context = Context<Self>;
+}
+
+// Register new session and assign unique id to this session
+impl Handler<Connect> for ChatServer {
+    type Result = usize;
+    fn handle(&mut self, msg: Connect, ctx: &mut Self::Context) -> Self::Result {
+        println!("Someone joined");
+        self.send_message("main", "Someone joined", 0);
+
+        // Register session with random id
+        let id = self.rng.gen::<usize>();
+        self.sessions.insert(id, msg.addr);
+
+        // auto join session to main room
+        self.rooms.entry("main".to_owned()).or_default().insert(id);
+
+        let count = self.visitor_count.fetch_add(1, Ordering::SeqCst);
+        self.send_message("main", &format!("Total visitors {count}"), 0);
+        id
+    }
+}
+
+// Handler for Disconnect message.
+impl Handler<Disconnect> for ChatServer {
+    type Result = ();
+    fn handle(&mut self, msg: Disconnect, _: &mut Context<Self>) {
+        println!("Someone disconnected");
+
+        let mut rooms: Vec<String> = Vec::new();
+        if self.sessions.remove(&msg.id).is_some() {
+            // remove session from all rooms
+            for (name, sessions) in &mut self.rooms {
+                if sessions.remove(&msg.id) {
+                    rooms.push(name.to_owned());
+                }
+            }
+        }
+        // send messages to another users
+        for room in rooms {
+            self.send_message(&room, "Someone dissconnected", 0);
+        }
+    }
+}
+
+// Handler for Message message
+impl Handler<ClientMessage> for ChatServer {
+    type Result = ();
+    fn handle(&mut self, msg: ClientMessage, _: &mut Context<Self>) {
+        self.send_message(&msg.room, msg.msg.as_str(), msg.id);
+    }
+}
+
+// Handler for `ListRooms` message.
+impl Handler<ListRooms> for ChatServer {
+    type Result = MessageResult<ListRooms>;
+    fn handle(&mut self, _: ListRooms, _: &mut Context<Self>) -> Self::Result {
+        let mut rooms = Vec::new();
+        for key in self.rooms.keys() {
+            rooms.push(key.to_owned())
+        }
+        MessageResult(rooms)
+    }
+}
+
+// Join room, send disconnect message to old rooms
+// send join message to new room
+impl Handler<Join> for ChatServer {
+    type Result = ();
+    fn handle(&mut self, msg: Join, _: &mut Context<Self>) {
+        let Join { id, name } = msg;
+        let mut rooms = Vec::new();
+        // remove session from all rooms
+        for (n, sessions) in &mut self.rooms {
+            if sessions.remove(&id) {
+                rooms.push(n.to_owned());
+            }
+        }
+        // send message to other users
+        for room in rooms {
+            self.send_message(&room, "Someone dissconneted", 0);
+        }
+        self.rooms.entry(name.clone()).or_default().insert(id);
+        self.send_message(&name, "Someone connected", id);
+    }
+}
+// Session
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+// How long before lack of client response causes a timeout
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
+struct WsChatSession {
+    pub id: usize,
+    pub hb: Instant,
+    pub room: String,
+    pub name: Option<String>,
+    pub addr: Addr<ChatServer>,
 }
